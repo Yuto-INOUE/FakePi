0ce0f36d77f40eaaf7619ac7f691356f34fe7652
Yuto Inoue <dsyuuto@gmail.com>
2022-12-14 18:00:45 +0900

    First release

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// (c) 2022 Yuto Inoue
+
+Console.WriteLine("π=");
+
+foreach (var c in CalculatePi())
+{
+	Console.Write(c);
+	Task.Delay(30).Wait();
+}
+
+static IEnumerable<string> CalculatePi()
+{
+	foreach (var c in "3.141592653589793238462643383279502884")
+	{
+		yield return c.ToString();
+	}
+
+	var rand = new Random();
+	while (true)
+	{
+		yield return rand.Next(10).ToString();
+	}
+}
